ee0ebd6399ac389fac516173fd058b508f697bc7
Efe Eroğlu <210541019@firat.edu.tr>
2024-10-11 20:05:31 +0300

    Teacher Auth işlemleri

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -6,7 +6,7 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
 # Öğrenci Giriş işlemi
-@auth_bp.route('/student/login', methods=['POST'])
+@auth_bp.route('/login/student', methods=['POST'])
 def studentLogin():
     data = request.get_json()
     tc = data.get('tc')
@@ -23,7 +23,7 @@ def studentLogin():
     return jsonify({'access_token': access_token, 'role': user.role}), 200
 
 # Öğrenci Kayıt İşlemi    
-@auth_bp.route('/student/register', methods=['POST'])
+@auth_bp.route('/register/student', methods=['POST'])
 def register_student():
     data = request.get_json()
     username = data.get('username')
@@ -47,3 +47,42 @@ def register_student():
     db.session.commit()
 
     return {"message": "Student registered successfully"}, 201
+
+
+@auth_bp.route('/register/teacher', methods=['POST'])
+def register_teacher():
+    data = request.get_json()
+    username = data.get('username')
+    lastname = data.get('lastname')
+    tc = data.get('tc')
+    teacher_number = data.get('teacher_number')
+    password = data.get('password')
+    subject = data.get('subject')
+
+    if Teacher.query.filter_by(tc=tc).first():
+        return {"error": "Bu Tc kimlik numarasına sahip başka bir öğretmen var"}, 400
+    if Teacher.query.filter_by(teacher_number=teacher_number).first():
+        return {"error": "Bu numarada farklı bir öğretmen var"}, 400
+
+    hashed_password = generate_password_hash(password)
+    new_teacher = Teacher(username=username, lastname=lastname, tc=tc,
+                          teacher_number=teacher_number, password=hashed_password,
+                          role='teacher', subject=subject)
+
+    db.session.add(new_teacher)
+    db.session.commit()
+
+    return {"message": "Teacher registered successfully"}, 201
+
+
+@auth_bp.route('/login/teacher', methods=['POST'])
+def login_teacher():
+    data = request.get_json()
+    tc = data.get('tc')
+    password = data.get('password')
+
+    teacher = Teacher.query.filter_by(tc=tc).first()
+    if not teacher or not check_password_hash(teacher.password, password):
+        return {"error": "Invalid credentials"}, 401
+
+    return {"message": "Login successful", "username": teacher.username}, 200
