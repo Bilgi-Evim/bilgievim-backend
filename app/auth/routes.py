8304846c38d172bcd20a5ccf2f7fdd39142da06e
Efe Eroğlu <210541019@firat.edu.tr>
2024-10-11 19:43:06 +0300

    Student auth işlemleri

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -2,24 +2,48 @@ from flask import jsonify, request
 from flask_jwt_extended import create_access_token
 from app.models import Teacher, Student, Admin
 from . import auth_bp
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
-# Login işlemi
-@auth_bp.route('/login', methods=['POST'])
-def login():
+# Öğrenci Giriş işlemi
+@auth_bp.route('/student/login', methods=['POST'])
+def studentLogin():
     data = request.get_json()
-    username = data.get('username')
+    tc = data.get('tc')
+    school_number = data.get('school_number')
     password = data.get('password')
 
-    user = Student.query.filter_by(username=username).first()
+    user = Student.query.filter_by(tc=tc).first()
 
-    if not user:
-        user = Teacher.query.filter_by(username=username).first()
-    if not user:
-        user = Admin.query.filter_by(username=username).first()
-
-    if not user or not user.password == password:
+    if not user or not user.password == password or not user.school_number == school_number:
         return jsonify({'error': 'Invalid credentials'}), 401
 
     access_token = create_access_token(identity=user.id)
     
     return jsonify({'access_token': access_token, 'role': user.role}), 200
+
+# Öğrenci Kayıt İşlemi    
+@auth_bp.route('/student/register', methods=['POST'])
+def register_student():
+    data = request.get_json()
+    username = data.get('username')
+    lastname = data.get('lastname')
+    tc = data.get('tc')
+    school_number = data.get('school_number')
+    password = data.get('password')
+    grade = data.get('grade', None)
+
+    if Student.query.filter_by(tc=tc).first():
+        return {"error": "Bu tc kimlik numarasına sahip bir öğrenci var"}, 400
+    if Student.query.filter_by(school_number=school_number).first():
+        return {"error": "Bu okul numarasına sahip bir kişi var"}, 400
+
+    hashed_password = generate_password_hash(password)
+    new_student = Student(username=username, lastname=lastname, tc=tc,
+                          school_number=school_number, password=hashed_password,
+                          role='student', grade=grade)
+    
+    db.session.add(new_student)
+    db.session.commit()
+
+    return {"message": "Student registered successfully"}, 201
