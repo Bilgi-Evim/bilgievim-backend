be25281011e32ba80539ec8d0c790e080bc9c2ed
Efe Eroğlu <210541019@firat.edu.tr>
2024-10-29 00:58:39 +0300

    additional claims

--- app/__init__.py
+++ app/__init__.py
@@ -3,19 +3,33 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from flask_swagger_ui import get_swaggerui_blueprint
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     
+    load_dotenv()
+    
     app.config.from_object('app.config.DevelopmentConfig')
     
     db.init_app(app)
     jwt = JWTManager(app)
     
-    CORS(app)
+    cors_origin = os.getenv('CORS_ORIGIN')
+    CORS(app, resources={r"/*": {"origins": cors_origin}})
     
+    SWAGGER_URL = '/swagger'  
+    API_URL = '/static/swagger.json' 
+    swaggerui_blueprint = get_swaggerui_blueprint(
+        SWAGGER_URL,
+        API_URL,
+        config={  
+            'app_name': "Bilgi Evim API"
+        }
+    )
     # Blueprint kayıtları
     from .auth.routes import auth_bp
     from .teacher.routes import teacher_bp
@@ -26,19 +40,6 @@ def create_app():
     app.register_blueprint(teacher_bp, url_prefix='/teacher')
     app.register_blueprint(student_bp, url_prefix='/student')
     app.register_blueprint(admin_bp, url_prefix='/admin') 
-
-    # Swagger Ayarları
-    SWAGGER_URL = '/swagger'  
-    API_URL = '/static/swagger.json' 
-    swaggerui_blueprint = get_swaggerui_blueprint(
-        SWAGGER_URL,
-        API_URL,
-        config={  
-            'app_name': "Bilgi Evim API"
-        }
-    )
-    
-    # Swagger blueprint'ini kaydedin
     app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
     
     return app
